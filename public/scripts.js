0379ee35dc896be47d5420b56885eada7e3d8dec
Jon-Emil <jer8@hi.is>
2025-01-29 16:27:35 +0000

    test

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,15 @@
-document.querySelectorAll("input[type='radio']").forEach((radio) => {
-    radio.addEventListener("change", function () {
-      let labels = this.closest("form").querySelectorAll(`[name="${this.name}"]`);
-  
-      labels.forEach((input) => {
-        input.parentElement.classList.remove("selected");
+function makeListeners() {
+    document.querySelectorAll("input[type='radio']").forEach((radio) => {
+        radio.addEventListener("change", function () {
+          let labels = this.closest("form").querySelectorAll(`[name="${this.name}"]`);
+        
+          labels.forEach((input) => {
+            input.parentElement.classList.remove("selected");
+          });
+      
+          this.parentElement.classList.add("selected");
+        });
       });
-  
-      this.parentElement.classList.add("selected");
-    });
-  });
-  
+}
+
+makeListeners();
